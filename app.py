59b64156ae4f13cf537124396ab3d2bff702d71b
Ajuda <hswf300@gmail.com>
2026-04-03 01:16:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,8 @@ app.config.update(
 )
 
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message = "Sessão expirada ou acesso restrito."
